a00c5cd094f02f3340a4fbbbe36226769b1334a0
Pierre Burgy <pierre.burgy@gmail.com>
2015-02-16 13:23:45 +0100

    update homepage : smooth scroll and css

--- js/main.js
+++ js/main.js
@@ -3,7 +3,39 @@
  */
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
 
+  // scroll - accueil-link
+  $("#accueil-link").click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, 800);
+  });
+
+  // scroll - invit-scroll
+  $("#invit-scroll").click(function () {
+    $('html, body').animate({
+      scrollTop: $("#street-session").offset().top
+    }, 800);
+  });
+
+  // scroll - streetLink
+  $("#street-link").click(function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $("#street-session").offset().top
+    }, 800);
+  });
+
+  // scroll - teamLink
+  $("#team-link").click(function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $("#team").offset().top
+    }, 800);
+  });
 });
